9d83688d616981d6e1f013001a3562ed7f8e2973
Akbermet Zhuma <akbermet.zh@gmail.com>
2021-12-26 21:09:13 +0600

    подправила немного код который есть у нас, как того просит документация

--- product/models.py
+++ product/models.py
@@ -31,10 +31,10 @@ class Product(models.Model):
         ('in stock', 'В наличии'),
         ('out of stock', 'Нет в наличии'),
     )
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
     description = models.TextField()
     image = models.ImageField(upload_to='products', null=True, blank=True)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = price = models.FloatField()
     availability = models.CharField(choices=CHOICES, max_length=20)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='products')
 
