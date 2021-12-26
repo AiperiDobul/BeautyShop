fc7902d8fa78830f616cfd9da31b666dd8abc35c
Akbermet Zhuma <akbermet.zh@gmail.com>
2021-12-26 22:33:24 +0600

    работающая версия карточки продукта, в которой есть ссылки для добвления продукта

--- product/urls.py
+++ product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', indexView, name='index')
+    path('', indexView, name='index'),
+    path('<int:pk>/', ProductDetailsView.as_view(), name='product-details'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from .models import *
+from django.views.generic.detail import DetailView
+from order.forms import AddToCartForm
 
 def indexView(request):
     products = Product.objects.all()
     return render(request, 'product/index.html', {'products': products})
+
+
+class ProductDetailsView(DetailView):
+    queryset = Product.objects.all()
+    template_name = 'product/product_details.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['cart_form'] = AddToCartForm()
+        return context
