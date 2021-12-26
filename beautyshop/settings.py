f4bfa238b67aa9ed8ed40de969da94e4e949c9c2
Akbermet Zhuma <akbermet.zh@gmail.com>
2021-12-26 20:57:22 +0600

    настроила настройки для использования django-shopping cart

--- beautyshop/settings.py
+++ beautyshop/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'phonenumber_field',
+    'cart',
 
     #my apps:
     'account',
@@ -71,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processor.cart_total_amount',
             ],
         },
     },
